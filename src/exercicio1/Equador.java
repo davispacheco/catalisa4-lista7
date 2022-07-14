ce4c96e15d9c28ebc27fb239e4d5631907e7e7ed
davispacheco <davipacheco1996@gmail.com>
2022-07-13 23:20:48 -0300

    Finalizar a adição do corpo à classe Equador.

--- src/exercicio1/Equador.java
+++ src/exercicio1/Equador.java
@@ -4,6 +4,8 @@ public class Equador extends Pais {
     public Equador() {
         this.setCodigoIso("ecu");
         this.setNome("Equador");
+        this.setPopulacao(7000000);
+        this.setDimensaoQuilometroQuadrado(3358941.032);
     }
 }
 }
